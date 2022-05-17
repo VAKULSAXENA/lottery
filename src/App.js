3ba518231b92514d8636eda929c83a96fb0b8539
vakul <abhisheksaxena714.vs@gmail.com>
2022-05-17 13:52:47 +0530

    web3 commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React,{ useEffect, useState } from 'react';
 import lottery from './lottery';
 import './App.css';
-import web3 from './web3';
+import Web3 from "web3";
 import {TailSpin} from 'react-loader-spinner';
 
 function App() {
@@ -12,6 +12,8 @@ function App() {
   const [value,setValue] = useState("")
   const [message,setMessage] = useState("")
   const [loader,setLoader] = useState("")
+  const[web3,setWeb3] = useState()
+  const[address,setAddress] = useState()
 
   const getOwner=async()=>{
     const owner = await lottery.methods.owner().call();
@@ -38,12 +40,25 @@ function App() {
   
   const onSubmit = async(e)=>{
     e.preventDefault();
-
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
+    if(typeof window !== 'undefined' && typeof window.ethereum !=='undefined'){
+      try {
+        await window.ethereum.request({method:"eth_requestAccounts"})
+        const web3 = new Web3(window.ethereum);
+        setWeb3(web3);
+        const accounts = await web3.eth.getAccounts();
+        setAddress(accounts[0]);
+        console.log(accounts);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }else{
+      console.log("Please install metamask");
+    }
+   
+  
     await lottery.methods.enter().send(
       {
-        from:accounts[0],
+        from:address[0],
         value:web3.utils.toWei(value,'ether'),
       }
     )
